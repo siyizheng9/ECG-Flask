1c21225ead3899e259332b0d6a4f015ebe2feb9a
siyi.zheng <siyi.zheng@aalto.fi>
2017-09-22 09:57:09 +0300

    add csv file management page

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import config
@@ -6,6 +7,7 @@ import flask_admin as admin
 from flask_pymongo import PyMongo
 from .models import DataView
 from .myadminview import downloadForm
+from flask_admin.contrib.fileadmin import FileAdmin
 from .data_download import download_data
 
 bootstrap = Bootstrap()
@@ -22,13 +24,23 @@ class MyAdminView(admin.BaseView):
         if request.method == 'POST':
             collection_name = form.collection.data
             print(request.method, collection_name)
-            data = download_data(mongo.db, collection_name)
-            return self.render('myadmin.html', words=data)
+            # data = download_data(mongo.db, collection_name)
+            download_data(mongo.db, collection_name)
+            filename = collection_name + '.csv'
+            return redirect(url_for('mydownloadview.download', path=filename))
         else:
             collections = mongo.db.collection_names()
             choices = [(c, c) for c in collections]
             form.collection.choices = choices
             return self.render('myadmin.html', form=form)
+
+
+class MyDownloadView(FileAdmin):
+    can_upload = False
+    can_delete = True
+    can_delete_dirs = True
+    can_mkdir = True
+    can_rename = True
 
 
 def create_app(config_name):
@@ -42,9 +54,13 @@ def create_app(config_name):
     myadmin.init_app(app)
     mongo.init_app(app)
 
+    path = os.path.join(os.path.dirname(__file__), 'data')
+    print('path:', path)
+
     with app.app_context():
         myadmin.add_view(DataView(mongo.db['kafkatopic']))
-        myadmin.add_view(MyAdminView(name="view1", category='download'))
+        myadmin.add_view(MyAdminView(name="Download"))
+        myadmin.add_view(MyDownloadView(path, name='Data file'))
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/data_download.py
+++ app/data_download.py
@@ -1,4 +1,13 @@
 
+import os
+
+path = os.path.join(os.path.dirname(__file__), 'data')
+
+
+def getPath(name):
+    return os.path.join(path, name + '.csv')
+
+
 def download_data(db, collection_name):
     print("dowloading", collection_name)
     collection = db[collection_name]
@@ -10,4 +19,13 @@ def download_data(db, collection_name):
         obj = next(iterator)
         data.append(obj['mMessage'])
     print(data)
+    write_to_file(data, collection_name)
     return data
+
+
+def write_to_file(data, collection_name):
+    file = open(getPath(collection_name), 'w')
+    for item in data:
+        file.write(item)
+        file.write("\n")
+    file.close
