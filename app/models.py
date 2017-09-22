43346658d652dc013a1fb01c28eeabdfa6e6f42a
siyi.zheng <siyi.zheng@aalto.fi>
2017-09-22 14:53:22 +0300

    added client_id in DataView

--- app/models.py
+++ app/models.py
@@ -4,8 +4,9 @@ from flask_admin.contrib.pymongo import ModelView
 
 class DataForm(form.Form):
     mMessage = fields.StringField('mMessage')
+    client_id = fields.SelectField('client_id')
 
 
 class DataView(ModelView):
-    column_list = ('_id', 'mMessage')
+    column_list = ('_id', 'client_id', 'mMessage')
     form = DataForm
