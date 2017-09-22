24e781815b0a46f2c3bb2b8f17c5a3ce14ef297a
siyi.zheng <siyi.zheng@aalto.fi>
2017-09-22 17:54:47 +0300

    fix bug

--- app/models.py
+++ app/models.py
@@ -1,12 +1,18 @@
 from wtforms import form, fields
 from flask_admin.contrib.pymongo import ModelView
+from flask_admin.contrib.pymongo.filters import FilterEqual, FilterLike
 
 
 class DataForm(form.Form):
     mMessage = fields.StringField('mMessage')
-    client_id = fields.SelectField('client_id')
+    client_id = fields.StringField('client_id')
 
 
 class DataView(ModelView):
-    column_list = ('_id', 'client_id', 'mMessage')
+    can_create = False
+    can_edit = True
+    column_filters = (
+        FilterEqual(column='client_id', name='client id'),
+        FilterLike(column='client_id', name='client id'))
+    column_list = ('client_id', 'mMessage')
     form = DataForm
